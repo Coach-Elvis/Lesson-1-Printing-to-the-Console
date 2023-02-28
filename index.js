fd599510071462bc6730f15768889c14f8643dda
Elvis <elvisgarcia93@gmail.com>
2023-02-28 00:36:19 -0500

    Include a Console Log and a Variable as a Preview to Lesson 2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello");
+
+// Example of variables (Covered in Lesson 2):
+let something = 'amazing';
+
+// Example of Printing Variable value to the Console (Covered in Lesson 2):
+console.log('something:', something);
+
+
