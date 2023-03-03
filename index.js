f9d4b5f2e44f7973674b24ab004a3301756f9aeb
Coach-Elvis <elvis.garcia@multiverse.io>
2023-03-02 21:02:30 -0500

    Include Comments for Reference Printing

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ let something = 'amazing';
 // Example of Printing Variable value to the Console (Covered in Lesson 2):
 console.log('something:', something);
 
+// Example of Printing Variable stored in a Reference
 let person = "Joe";
 console.log(person);
